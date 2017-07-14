8169d2787c4642bd3e545062d0fca40f9d21fe9f
Guillermo Lopez <guillermolopez@pawadominicana.com>
2017-07-14 18:16:52 -0400

    project model completed

--- models/index.js
+++ models/index.js
@@ -1,11 +1,11 @@
-const sequelizeConfig = require('../lib/sequelize')
-
-const { sequelize, Sequelize } = sequelizeConfig
+const Sequelize = require('sequelize')
+const sequelize = require('../lib/sequelize')
 
 const Project = sequelize.define('project', {
   id: {
     type: Sequelize.CHAR(36),
-    defaultValue: Sequelize.UUIDV4
+    defaultValue: Sequelize.UUIDV4,
+    primaryKey: true
   },
   name: Sequelize.STRING,
   description: Sequelize.STRING
