a7adc21cf7dd434c8c74b5a03b5e04310406ede4
Guillermo Lopez <guillermolopez@pawadominicana.com>
2017-07-14 17:12:37 -0400

    working with intranet-db

---
+++ config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  dbName: process.env.DB_NAME || 'intranet',
+  dbUser: process.env.DB_USER || 'root',
+  dbPassword: process.env.DB_PASSWORD || '',
+  extra: {
+    host: process.env.DB_HOST || 'localhost',
+    dialect: process.env.DB_DIALECT || 'mysql',
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    }
+  }
+}
---
+++ test/db-test.js
@@ -0,0 +1,5 @@
+import test from 'ava'
+
+test('Should be pass', t => {
+  t.pass()
+})
